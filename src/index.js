95d5207c54c77935826680fd3e62cce0914ec83a
victor-val <vval26v@gmail.com>
2023-02-20 11:35:17 +0100

    showing the past moves

--- src/index.js
+++ src/index.js
@@ -77,6 +77,18 @@ import './index.css';
       const history = this.state.history;
       const current = history[history.length-1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ? 
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        )
+      });
+
       let status;
       if(winner){
         status = 'Winner: ' + winner;
@@ -93,7 +105,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
